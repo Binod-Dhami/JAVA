0f224adc21fe1a11a72a2dee3890b79b21387dd3
Binod-Dhami <binodhami333@gmail.com>
2022-12-18 23:21:05 +0545

    new

---
+++ Radio_Button/CheckBox.java
@@ -0,0 +1,11 @@
+package Radio_Button;
+import javax.swing.*;
+
+import window_Listener.Actionlistener;
+
+import java.awt.event.*;
+
+public class CheckBox extends JFrame implements Actionlistener {
+    JRadioCh
+    
+}
