d7ac22a8ea1971baf99258ec129af8aa9bdcc5ae
Binod-Dhami <binodhami333@gmail.com>
2022-12-19 19:40:25 +0545

    not completed

--- Radio_Button/CheckBox.java
+++ Radio_Button/CheckBox.java
@@ -1,11 +1,47 @@
 package Radio_Button;
 import javax.swing.*;
-
-import window_Listener.Actionlistener;
-
+//import java.awt.*;
 import java.awt.event.*;
 
-public class CheckBox extends JFrame implements Actionlistener {
-    JRadioCh
+
+public class CheckBox extends JFrame implements ItemListener {
+    JCheckBox java,c;
+    public CheckBox(){
+        setSize(500,700);
+        java=new JCheckBox("java",true);
+        c=new JCheckBox("c++",true);
+        add(java);add(c);
+        setLayout(null);
+        setVisible(true);
+        java.addItemListener(this);
+        c.addItemListener(this);
+
+    }
+    @Override
+    public void itemStateChanged(ItemEvent e)
+    {
+        // if the state of java is changed
+        if (e.getSource() == java) {
+            if (e.getStateChange() == 1){
+            JOptionPane.showMessageDialog(this, "java is  selected");
+             } else{
+            JOptionPane.showMessageDialog(this,"java is  not selected");
+        }
+    }
+  
+        // if the state of c++ is changed
+        else {
+            if (e.getStateChange() == 1){
+            JOptionPane.showMessageDialog(this," c++ is  selected");
+             }
+              else{
+            JOptionPane.showMessageDialog(this,"c++ not selected");
+        }
+    }
+    }
+    
+    public static void main(String args[]){
+        new CheckBox();
+    }
     
 }
