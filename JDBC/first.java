b62d5ccf27e0e8a9efdd8f63c994dd921361771e
Binod-Dhami <binodhami333@gmail.com>
2022-12-22 21:05:53 +0545

    first try JDBC

---
+++ JDBC/first.java
@@ -0,0 +1,23 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class first {
+    public static void main (String[]args){
+
+   try{
+    Class.forName();
+    Connection con=DriverManager();
+    Statement smt=con.createStatement();
+    smt.executeUpdate();
+    System.out.println();
+    con.close()
+   }
+   catch(Exception e){
+    System.out.print(e);
+   }
+    
+}
+}
