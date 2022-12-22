9800c8167fbfbf6b583840a6dd99b70192a2e3b4
Binod-Dhami <binodhami333@gmail.com>
2022-12-22 22:19:19 +0545

    strings changes

--- JDBC/first.java
+++ JDBC/first.java
@@ -8,12 +8,12 @@ public class first {
     public static void main (String[]args){
 
    try{
-    Class.forName();
-    Connection con=DriverManager();
+    Class.forName("");
+    Connection con=DriverManager.getConnection("");
     Statement smt=con.createStatement();
-    smt.executeUpdate();
+    smt.executeUpdate("");
     System.out.println();
-    con.close()
+    con.close();
    }
    catch(Exception e){
     System.out.print(e);
