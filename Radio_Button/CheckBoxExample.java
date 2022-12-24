f001015e5b62707d77d6826790bfe24a887ef533
Binod-Dhami <binodhami333@gmail.com>
2022-12-24 19:51:10 +0545

    c

---
+++ Radio_Button/CheckBoxExample.java
@@ -0,0 +1,29 @@
+package Radio_Button;
+
+import java.awt.*; 
+ class CheckBoxExample extends Frame 
+ { 
+      CheckBoxExample() 
+     { 
+         setLayout(new FlowLayout());  
+         Label lblHobbies = new Label("HOBBIES"); 
+         Checkbox chkSports = new Checkbox("Sports"); 
+         Checkbox chkMusic = new Checkbox("Music ",true); 
+         Checkbox chkReading = new Checkbox("Reading"); 
+         add(lblHobbies); 
+         add(chkSports); 
+         add(chkMusic); 
+         add(chkReading); 
+     } 
+ } 
+    class CheckboxJavaExample 
+   { 
+          public static void main(String args[]) 
+         { 
+                 CheckBoxExample frame = new CheckBoxExample(); 
+                 frame.setTitle("Checkbox in Java Example"); 
+                 frame.setSize(350,100); 
+                 frame.setResizable(false); 
+                 frame.setVisible(true); 
+          } 
+   } 
