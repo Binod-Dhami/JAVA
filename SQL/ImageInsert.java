8bc399adc33b1c67f9b2ea4b39d6510ef843341f
Binod-Dhami <binodhami333@gmail.com>
2023-01-08 22:29:00 +0545

    create database

--- SQL/ImageInsert.java
+++ SQL/ImageInsert.java
@@ -11,15 +11,15 @@ public class ImageInsert {
 
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/database_name";
+        String url = "jdbc:mysql://localhost:3306/college";
         String username = "root";
-        String password = "password";
+        String password = "";
 
-        String imagePath = "C:\\image.jpg";
+        String imagePath = "/Users/binoddhami/Desktop/Java/SQL/image.png";
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
 
-            String sql = "INSERT INTO images (image) VALUES (?)";
+            String sql = "INSERT INTO ncit (ncit) VALUES (?)";
 
             try (PreparedStatement statement = connection.prepareStatement(sql)) {
 
