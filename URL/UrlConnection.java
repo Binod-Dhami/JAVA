36e11541fcd4d4f625de6fe33a4b76585308f4b3
Binod-Dhami <binodhami333@gmail.com>
2023-01-10 22:16:45 +0545

    c

--- URL/UrlConnection.java
+++ URL/UrlConnection.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class UrlConnection {
     public static void main(String []args)
     throws IOException{
-        URL url=new URL("https://www.javatpoint.com/URLConnection-class");
+        URL url=new URL("https://www.google.com.np/");
         URLConnection cn=url.openConnection();
         InputStream in=cn.getInputStream();
         int content;
